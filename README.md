20b5d978b3594fd4d4973e246ee35d49e5215e92
DevKiD <75792299+DevKiDMediaCreation@users.noreply.github.com>
2023-09-15 21:54:38 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# cosmolangarchitecture-CosArch" 
+"# cosmolangarchitecture-CosArch" 
--- shell/+$*~#adoin32vrcmasc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p build  
-cmake -S . -B build/
-cmake --build build/
-./build/cosarch ./test.cos
-# echo $?
