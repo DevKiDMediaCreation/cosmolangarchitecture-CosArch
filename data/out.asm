543903a2677aab0ec37ab6c8905b28605064df26
DevKiD <75792299+DevKiDMediaCreation@users.noreply.github.com>
2023-09-18 19:07:16 +0200

    Delete the mode for more input like <input.cos> <build target> <run options>

--- data/out.asm
+++ data/out.asm
@@ -1,5 +1,5 @@
 global _start
 _start:
      mov rax, 60
-     mov rdi, 234
+     mov rdi, 256
      syscall
--- src/main.cpp
+++ src/main.cpp
@@ -35,13 +35,6 @@ int main(int argc, char *argv[]) {
 
     system("rm -fr ../data && mkdir -p ../data");
 
-    /**if (argv[2] != nullptr) {
-        std::string command = "rm -fr " + std::string(argv[2]) + " && mkdir -p " + std::string(argv[2]);
-        system(command.c_str());
-    } else {
-        system("rm -fr ../data && mkdir -p ../data");
-    }*/
-
     Tokenizer tokenizer(std::move(contents));
     std::vector<Token> tokens = tokenizer.tokenize();
 
@@ -55,45 +48,13 @@ int main(int argc, char *argv[]) {
 
     Generator generator(tree.value());
     {
-        /**if (argv[2] != nullptr) {
-            std::fstream file(std::string(argv[2]) + "/out.asm", std::ios::out);
-            file << tokens_to_asm(tokens);
-        } else {
-            std::fstream file("../data/out.asm", std::ios::out);
-            file << tokens_to_asm(tokens);
-        }*/
         std::fstream file("../data/out.asm", std::ios::out);
         file << generator.generate();
     }
 
     system("nasm -f elf64 ../data/out.asm -o ../data/out.o && ld ../data/out.o -o ../data/out");
 
-    // For later
-    /**if(argv[3] == "--debug") {
-        std::cout << tokens_to_asm(tokens) << std::endl;
-    }*/
-
-    /**if (argv[2] != nullptr) {
-        std::string command =
-                "nasm -f elf64 " + std::string(argv[2]) + " /out.asm -o " + std::string(argv[2]) + "/out.o && ld " +
-                std::string(argv[2]) + "/out.o -o " + std::string(argv[2]) + "/out";
-        system(command.c_str());
-    } else {
-        system("nasm -f elf64 ../data/out.asm -o ../data/out.o && ld ../data/out.o -o ../data/out");
-    }*/
-
-    /**if(argv[2] == "-r") {
-        system("../data/out && echo $?");
-    }*/ // Add a -r run option to run the program after compiling
-
     std::cout << "Compiling successfully. File build: " << argv[1] << std::endl;
-
-    /**if (argv[3] == "-r") {
-        if (argv[2] == nullptr) {
-            system("../data/out && echo $?");
-        } else {
-        }
-    }*/
 
     return EXIT_SUCCESS;
 }
